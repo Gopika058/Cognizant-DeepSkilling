d0a6e14feb3a1ac1375145f169a822afd63576f9
Gopika <147249550+Gopika058@users.noreply.github.com>
2025-07-08 17:49:30 +0530

    Adding week 3 hands-on

---
+++ Week3_Spring Core and Maven/04_Creating and Configuring/code/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+
+package com.library.repository;
+
+public class BookRepository {
+    public void save(String title) {
+        System.out.println("BookRepository: Saved book - " + title);
+    }
+}
